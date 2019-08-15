295716880b2539cde01d0521d9264805dfec3df4
Malcolm Bouzi <malcolmbouzi@Malcolms-Air.home>
2019-08-15 12:28:37 -0400

    add filter

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,18 @@
-function feed(parent, args, context, info) {
-  return context.prisma.posts()
+async function feed(parent, args, context, info) {
+  const where = args.filter ? {
+   OR: [
+     { content_contains: args.filter }
+     { deleted: false },
+   ],
+ } :  
+  OR: [
+    { deleted: false },
+  ],
+}
+
+ const posts = await context.prisma.posts({where})
+
+ return posts
 }
 
 module.exports = {
