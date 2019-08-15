f9182e668da5357d06773cd519cae35049b9aa8d
Malcolm Bouzi <malcolmbouzi@Malcolms-Air.home>
2019-08-15 11:53:14 -0400

    auth set up

--- src/index.js
+++ src/index.js
@@ -1,34 +1,28 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('./generated/prisma-client')
 
-let posts = [{
-  id: 'post-0',
-  content: 'dummy post'
-}]
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
-let idCount = posts.length
 
 const resolvers = {
-  Query: {
-    info: () => 'This is the API',
-    feed: (root, args, context, info) => {
-      return context.prisma.posts()
-    }
-  },
-  Mutation: {
-    post: (root, args, context) => {
-      return context.prisma.createPost({
-        content: args.content,
-        deleted: args.deleted
-      })
-    }
-  }
+  Query,
+  Mutation,
+  User,
+  Link
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: { prisma },
+  context: request => {
+    return {
+      ...request,
+      prisma,
+    }
+  },
 })
 
 server.start(() => console.log('Server is running on http://localhost:4000'))
